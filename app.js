aa8217c493ad2a64ec28a348fdd7024c449a2866
anuragsrma <anuragsharma0140@gmail.com>
2023-12-17 10:52:00 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const http = require('http');
+
+const express = require('express');
+
+const app = express();
+
+app.use((req,res,next) =>{
+   console.log("in the middleware");
+   next();
+});
+
+
+app.use((req,res,next) =>{
+    console.log("in another middleware");
+    res.send('<h1>Hello in express!<h1>')
+ });
+ 
+
+// const server = http.createServer(app);
+
+// server.listen(3000);
+app.listen(3000);
