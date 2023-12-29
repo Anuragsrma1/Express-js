c3a3e15d4255059bd5f07663487e062ac3f52fb7
anuragsrma <anuragsharma0140@gmail.com>
2023-12-29 13:29:40 +0530

    Serving HTML , CSS and JS (VIEWS)

--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,41 @@
+const path = require('path');
+
 const express = require('express');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 //   /admin/add-product => get
 router.get('/add-product', (req , res, next) => {
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>');
-    
+    res.sendFile(path.join(rootDir , 'views' , 'add-product.html'));
  });
 
+router.get('/ContactUs' , (req,res,next) =>{
+  res.sendFile(path.join(rootDir,'views', 'contact.html'));
+  
+})
+
+router.get('/success' , (req,res,next) =>{
+  res.sendFile(path.join(rootDir,'views', 'success.html'));
+  
+})
 //  /admin/add-product => Post
 
  router.post('/add-product' ,(req,res,next) => {
    console.log(req.body); 
    res.redirect('/');
  })
+
+ router.post('/ContactUs', (req,res,next) => {
+  console.log(req.body);
+  res.redirect('/success');
+ console.log("Form successfuly filled");
+ }) 
+
+ router.post('/success' ,(req,res,next) => {
+  console.log(req.body); 
+ // res.redirect('/');
+})
 
 module.exports = router;
